9b19f538023140a7af9387ae640f9bd26b476325
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-02-03 14:53:09 +0700

    Update code

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -12,14 +12,14 @@
     <title>{{ $pageTitle }}</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('public/backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('public/backend/css/sb-admin-2.min.css') }}" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="{{asset('public/admin/vendor/datatables/dataTables.bootstrap4.min.css')}}"> --}}
+    <link href="{{ asset('backend/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{asset('admin/vendor/datatables/dataTables.bootstrap4.min.css')}}"> --}}
     <link rel="stylesheet" href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
     <!-- Link to SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
@@ -89,27 +89,28 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('public/backend/js/script.js') }}"></script>
-    <script src="{{ asset('public/backend/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('public/backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/js/script.js') }}"></script>
+    <script src="{{ asset('backend/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('public/backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ asset('public/backend/js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
-    <script src="{{ asset('public/backend/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/chart.js/Chart.min.js') }}"></script>
     <!-- Page level custom scripts -->
-    <script src="{{ asset('public/backend/js/demo/chart-area-demo.js') }}"></script>
-    <script src="{{ asset('public/backend/js/demo/chart-pie-demo.js') }}"></script>
-    <script src="{{ asset('public/plugins/ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ asset('backend/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('backend/js/demo/chart-pie-demo.js') }}"></script>
+    <script src="{{ asset('plugins/ckeditor/ckeditor.js') }}"></script>
 
     <script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <!-- SweetAlert2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="{{ asset('public/vendor/laravel-filemanager/js/stand-alone-button.js') }}"></script>
+    <script src="{{ asset('vendor/laravel-filemanager/js/stand-alone-button.js') }}"></script>
+
     @yield('script')
 </body>
 
